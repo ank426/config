e06967daa4f998b9dc2037707f7d1a9990118a2e
Ankit Avinash <ankit.avinash@protonmail.com>
2024-09-06 23:11:01 +0530

    bin

--- bin/brightness-up.sh
+++ bin/brightness-up.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-brightness=$($XDG_CONFIG_HOME/hypr/scripts/get-exp-brightness-round-5.sh)
+brightness=$($XDG_CONFIG_HOME/bin/get-exp-brightness-round-5.sh)
 if [ "$brightness" -ne 0 ]; then
     brightnessctl -e -q set $(($brightness + 5))%
 else
