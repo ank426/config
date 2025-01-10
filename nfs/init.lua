2fd19e6279d6689ca0bec64cad807fa81b37044d
Ankit Avinash <ankit.avinash@protonmail.com>
2025-01-10 15:50:03 +0530

    nfs

---
+++ nfs/init.lua
@@ -0,0 +1,14 @@
+vim.opt.breakindent = true
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.inccommand = "split"
+vim.opt.list = true
+vim.opt.listchars = { trail = "~" }
+vim.opt.matchpairs:append("<:>")
+vim.opt.mouse = ""
+vim.opt.nrformats:append("unsigned")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 999
+vim.opt.shiftwidth = 2
+vim.opt.showbreak = "+++ "
