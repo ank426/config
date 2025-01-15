56fc7fef7ab034d0cf51e6d38c3aaa38d42eef14
Ankit Avinash <ankit.avinash@protonmail.com>
2025-01-15 17:52:54 +0530

    update

---
+++ nfs/lua/plugins/lazygit.lua
@@ -0,0 +1,13 @@
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  keys = {
+    { "<leader>lg", vim.cmd.LazyGit, desc = "LazyGit" },
+  },
+}
