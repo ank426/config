5169a63089653d5707ef8e337640d7e66595bd83
Ankit Avinash <ankit.avinash@protonmail.com>
2025-01-16 12:24:52 +0530

    update

--- nfs/lua/plugins/treesitter.lua
+++ nfs/lua/plugins/treesitter.lua
@@ -1,17 +1,24 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = "BufEnter",
-  opts = {
-    auto_install = true,
-    highlight = { enable = true },
-    incremental_selection = { enable = true },
-    indent = { enable = true },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-    vim.treesitter.language.register("bash", "zsh")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = "BufEnter",
+    opts = {
+      auto_install = true,
+      highlight = { enable = true },
+      incremental_selection = { enable = true },
+      indent = { enable = true },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      vim.treesitter.language.register("bash", "zsh")
 
-    vim.hl = vim.highlight -- Remove when nvim>=10.4
-  end,
+      vim.hl = vim.highlight -- Remove when nvim>=10.4
+    end,
+  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter-textobjects",
+  --   dependencies = {"nvim-treesitter/nvim-treesitter"},
+  -- },
+  -- There is also nvim-treesitter-subjects
 }
