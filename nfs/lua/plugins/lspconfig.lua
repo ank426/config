2fd2f25618daa590e65eb6f8729b135ffe05f878
Ankit Avinash <ankit.avinash@protonmail.com>
2025-01-18 22:08:10 +0530

    update

--- nfs/lua/plugins/lspconfig.lua
+++ nfs/lua/plugins/lspconfig.lua
@@ -1,6 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
-  event = "VeryLazy", -- Gives problems with BufReadPre and automatic_installation
+  -- event = "VeryLazy", -- Any form of lazy loading seems to give random problems
   dependencies = {
     { "williamboman/mason.nvim",           opts = {} },
     { "williamboman/mason-lspconfig.nvim", opts = { automatic_installation = true } },
@@ -33,5 +33,56 @@ return {
         },
       },
     })
+
+    lspconfig.basedpyright.setup({
+      on_init = function(client)
+        client.server_capabilities.inlayHintProvider = false
+        client.server_capabilities.textDocumentSync = nil
+      end,
+    })
+    lspconfig.ruff.setup({
+      on_init = function(client)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+      init_options = {
+        settings = {
+          lineLength = 150,
+          configurationPreference = "filesystemFirst",
+          lint = {
+            select = {   -- https://docs.astral.sh/ruff/rules/
+              "F",       -- Pyflakes
+              "E112",    -- no-indented-block
+              "E113",    -- unexpected-indentation
+              "E203",    -- whitespace-before-punctuation
+              "E272",    -- multiple-spaces-before-keyword
+              "E275",    -- missing-whitespace-after-keyword
+              "E303",    -- too-many-blank-lines
+              "E304",    -- blank-line-after-decorator
+              "E501",    -- line-too-long
+              "E702",    -- multiple-statements-on-one-line-semicolon
+              "E703",    -- useless-semicolon
+              "E731",    -- lambda-assignment
+              "W191",    -- tab-indentation
+              "W291",    -- trailing-whitespace
+              "W293",    -- blank-line-with-whitespace
+              "UP039",   -- unnecessary-class-parentheses
+              "C416",    -- unnecessary-comprehension
+              "RET506",  -- superfluous-else-raise
+              "RET507",  -- superfluous-else-continue
+              "A",       -- builtin-variable-shadowing, builtin-argument-shadowing, builtin-attribute-shadowing
+              "SIM105",  -- suppressible-exception
+              "FURB110", -- if-exp-instead-of-or-operator
+              "RUF018",  -- assignment-in-assert
+            },
+          },
+        },
+      },
+    })
   end,
 }
+
+-- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/
+-- search ServerCapabilites 2nd match
+-- I didn't read, just tried out random stuff + common sense
+-- Also, no idea why it is server capabilities and not client
