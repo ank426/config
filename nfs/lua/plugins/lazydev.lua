22cd52041945f2da7331f733f46f25aa17ddd3cb
Ankit Avinash <ankit.avinash@protonmail.com>
2025-01-19 23:26:16 +0530

    update

--- nfs/lua/plugins/lazydev.lua
+++ nfs/lua/plugins/lazydev.lua
@@ -3,16 +3,13 @@ return {
   ft = "lua",
   opts = {
     library = {
+      "lazy.nvim",
       { path = "${3rd}/luv/library", words = { "vim%.uv" } },
     },
     enabled = function(root_dir)
-      if vim.g.lazydev_enabled == false then
-        return false
-      end
+      if vim.g.lazydev_enabled == false then return false end
       for _, rtp_dir in ipairs(vim.opt.runtimepath:get()) do
-        if root_dir:sub(1, #rtp_dir) == rtp_dir then
-          return true
-        end
+        if root_dir:sub(1, #rtp_dir) == rtp_dir then return true end
       end
       return false
     end,
