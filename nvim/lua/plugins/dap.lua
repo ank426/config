34e37ab8f763b7b6bc729fcc745c5b5b509b71a0
Ankit Avinash <ankit.avinash@protonmail.com>
2025-01-24 23:16:20 +0530

    update

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,3 +1,11 @@
 return {
   "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
+    dap.adapters.py = {
+      type = "executable",
+      command = "/usr/bin/python",
+      args = {"-m", "debuygpy.adapter"},
+    }
+  end,
 }
